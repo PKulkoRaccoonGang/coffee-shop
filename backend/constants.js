63ece3ab410c2c2b51a209e18f8d4461650c9015
Peter Kulko <93188219+PKulkoRaccoonGang@users.noreply.github.com>
2023-11-12 12:21:04 +0200

    refactor: removed backend folder (#27)
    * refactor: backend refactoring
    
    * refactor: code refactoring
    
    * refactor: deleted backand dir

--- backend/constants.js
+++
@@ -1,5 +0,0 @@
-const MONGODB_URI = 'mongodb+srv://peterkulko:IOeJH2uyQBU0zDDj@cluster0.dakktrl.mongodb.net/coffee-shop?retryWrites=true&w=majority';
-const USER_ID = '654686239c1d584cd2e32f38';
-const PORT = 4444;
-
-module.exports = { MONGODB_URI, PORT, USER_ID };
